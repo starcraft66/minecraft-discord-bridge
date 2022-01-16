904524508129d561b971217722290daa5ab674c7
Tristan Gosselin-Hane <starcraft66@gmail.com>
2022-01-16 01:01:49 -0500

    config: support loading conf values from env vars

--- minecraft_discord_bridge/config.py
+++ minecraft_discord_bridge/config.py
@@ -19,6 +19,14 @@
 
 import json
 import logging
+import os
+
+
+def get_config_value(json_config, section, key):
+    env_value = os.getenv("MDB_" + section.upper() + "_" + key.upper(), None)
+    if env_value is not None:
+        return env_value
+    return json_config[section.upper()][key.upper()]
 
 
 class Configuration(object):
@@ -28,29 +36,29 @@ class Configuration(object):
             with open(path, 'r', encoding="utf-8") as file:
                 self._config = json.load(file)
             if self._config:
-                self.mc_username = self._config["MAIN"]["MC_USERNAME"]
-                self.mc_password = self._config["MAIN"]["MC_PASSWORD"]
-                self.mc_server = self._config["MAIN"]["MC_SERVER"]
-                self.mc_port = self._config["MAIN"]["MC_PORT"]
-                self.mc_online = self._config["MAIN"]["MC_ONLINE"]
-                self.discord_token = self._config["MAIN"]["DISCORD_APP_TOKEN"]
-                self.logging_level = self._config["MAIN"]["LOG_LEVEL"]
-                self.message_delay = self._config["MAIN"]["MESSAGE_DELAY"]
-                self.failsafe_retries = self._config["MAIN"]["FAILSAFE_RETRIES"]
-                self.vanilla_chat_mode = self._config["MAIN"]["VANILLA_CHAT_MODE"]
-                self.admin_users = self._config["MAIN"]["ADMINS"]
-                self.auth_ip = self._config["AUTH_SERVER"]["BIND_IP"]
-                self.auth_port = self._config["AUTH_SERVER"]["PORT"]
-                self.auth_dns = self._config["AUTH_SERVER"]["DNS_WILDCARD"]
-                self.database_connection_string = self._config["DATABASE"]["CONNECTION_STRING"]
-                self.es_enabled = self._config["ELASTICSEARCH"]["ENABLED"]
-                self.es_url = self._config["ELASTICSEARCH"]["URL"]
-                self.es_auth = self._config["ELASTICSEARCH"]["AUTH"]
-                self.es_username = self._config["ELASTICSEARCH"]["USERNAME"]
-                self.es_password = self._config["ELASTICSEARCH"]["PASSWORD"]
-                self.debugging_enabled = self._config["DEBUGGING"]["ENABLED"]
-                self.debugging_ip = self._config["DEBUGGING"]["BIND_IP"]
-                self.debugging_port = self._config["DEBUGGING"]["PORT"]
+                self.mc_username = get_config_value(self._config, "MAIN", "MC_USERNAME")
+                self.mc_password = get_config_value(self._config, "MAIN", "MC_PASSWORD")
+                self.mc_server = get_config_value(self._config, "MAIN", "MC_SERVER")
+                self.mc_port = get_config_value(self._config, "MAIN", "MC_PORT")
+                self.mc_online = get_config_value(self._config, "MAIN", "MC_ONLINE")
+                self.discord_token = get_config_value(self._config, "MAIN", "DISCORD_APP_TOKEN")
+                self.logging_level = get_config_value(self._config, "MAIN", "LOG_LEVEL")
+                self.message_delay = get_config_value(self._config, "MAIN", "MESSAGE_DELAY")
+                self.failsafe_retries = get_config_value(self._config, "MAIN", "FAILSAFE_RETRIES")
+                self.vanilla_chat_mode = get_config_value(self._config, "MAIN", "VANILLA_CHAT_MODE")
+                self.admin_users = get_config_value(self._config, "MAIN", "ADMINS")
+                self.auth_ip = get_config_value(self._config, "AUTH", "SERVER_BIND_IP")
+                self.auth_port = get_config_value(self._config, "AUTH", "SERVER_PORT")
+                self.auth_dns = get_config_value(self._config, "AUTH", "SERVER_DNS_WILDCARD")
+                self.database_connection_string = get_config_value(self._config, "DATABASE", "CONNECTION_STRING")
+                self.es_enabled = get_config_value(self._config, "ELASTICSEARCH", "ENABLED")
+                self.es_url = get_config_value(self._config, "ELASTICSEARCH", "URL")
+                self.es_auth = get_config_value(self._config, "ELASTICSEARCH", "AUTH")
+                self.es_username = get_config_value(self._config, "ELASTICSEARCH", "USERNAME")
+                self.es_password = get_config_value(self._config, "ELASTICSEARCH", "PASSWORD")
+                self.debugging_enabled = get_config_value(self._config, "DEBUGGING", "ENABLED")
+                self.debugging_ip = get_config_value(self._config, "DEBUGGING", "BIND_IP")
+                self.debugging_port = get_config_value(self._config, "DEBUGGING", "PORT")
             else:
                 self.logger.error("error reading config")
                 exit(1)
